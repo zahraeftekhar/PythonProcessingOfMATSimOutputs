b0adffb76feab7fab704f6cc7b056a80d997cd4a
zahra <53484857+zahraeftekhar@users.noreply.github.com>
2020-05-15 11:28:09 +0200

    changing the xmldoc names to plan and experienced

--- combineExperiencedNormalPlan1.py
+++ combineExperiencedNormalPlan1.py
@@ -2,10 +2,10 @@
 # importing XML file plan
 from xml.dom import minidom
 xmlPlan = minidom.parse("C:/Users/zahraeftekhar/eclipse-workspace/matsim-code-examples/Results_PlanWithOnlyCar_30secSnapShot/ITERS/it.1/1.plans.xml")
-itemlist = xmldoc.getElementsByTagName('person')
-del xmldoc
+itemlist = xmlPlan.getElementsByTagName('person')
+del xmlPlan
 # importing XML file experienced plan
 from xml.dom import minidom
 xmlExperienced = minidom.parse("C:/Users/zahraeftekhar/eclipse-workspace/matsim-code-examples/Results_PlanWithOnlyCar_30secSnapShot/ITERS/it.1/1.experienced_plans.xml")
-itemlist = xmldoc.getElementsByTagName('person')
-del xmldoc
+itemlist = xmlExperienced.getElementsByTagName('person')
+del xmlExperienced
